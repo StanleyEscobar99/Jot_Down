9b68f2e9d96245a9cfec21caf9b7b0e4793e71dd
StanleyEscobar99 <stanley.escobar.99@gmail.com>
2023-01-03 13:11:55 -0500

    Completed Backend

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     roles: [{
         type: String,
-        default: "Member"
+        default: "Employee"
     }],
     active: {
         type: Boolean,
